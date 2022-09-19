e489836ad4fa3dd7f4db2e236319f8b6a9513451
ssolarte-PUJ <ssolarteserna@javerianacali.edu.co>
2022-09-19 17:03:47 -0500

    MOD: Añadir esqueleto application.h

--- Application.h
+++ Application.h
@@ -1,6 +1,14 @@
 #ifndef TAREA_HUESPEDES_TEAM_MUSLO_APPLICATION_H
 #define TAREA_HUESPEDES_TEAM_MUSLO_APPLICATION_H
 
+#include <iostream>
+#include <unordered_map>
+#include "Evaluation.h"
+#include "Person.h"
+#include "Reservation.h"
+#include "Owner.h"
+#include "Owner.h"
+
 
 
 class Application {
