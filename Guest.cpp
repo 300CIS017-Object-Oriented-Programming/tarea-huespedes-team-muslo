06ff0361303e776c0025ea64ca592b479336b500
nicolasvalencia <nvalenciapayan@gmail.com>
2022-09-20 06:52:14 -0500

    NEW: Constructores y funciones base de Guest

--- Guest.cpp
+++ Guest.cpp
@@ -1,1 +1,30 @@
 #include "Guest.h"
+
+#include <iostream>
+
+Guest::Guest(){
+    infoHospital = "";
+    city = "";
+}
+
+void Guest::printDataGuest() {
+    cout << "Info Hospital: " << infoHospital << "\n";
+    cout << "City: " << city << "\n";
+}
+
+string Guest::getInfoHospital() {
+    return infoHospital;
+}
+
+void Guest::setInfoHospital() {
+    Guest::infoHospital = infoHospital;
+}
+
+string Guest::getCity() {
+    return city;
+}
+
+void Guest::setCity() {
+    Guest::city = city;
+}
+
--- Guest.h
+++ Guest.h
@@ -11,6 +11,16 @@ class Guest{
 private:
     string infoHospital;
     string city;
+public:
+    Guest();
+
+    void printDataGuest();
+
+    string getInfoHospital();
+    void setInfoHospital();
+
+    string getCity();
+    void setCity();
 };
 
 #endif //TAREA_HUESPEDES_TEAM_MUSLO_GUEST_H
